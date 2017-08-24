dac4a287d06dfda8d27b1440e28f342e1070958f
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-24 15:44:03 -0400

    complete omniauth facebook in model and routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
   has_many :user_movies
   has_many :movies, :through => :user_movies
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
