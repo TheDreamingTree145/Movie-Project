be1f44183cbd1471d899c268578251f416e9ff4a
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-25 18:08:45 -0400

    associations and migrations, not migrated

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :user_movies
+  has_many :users, :through => :user_movies
+  has_many :characters
+  has_many :actors, :through => :characters
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :characters
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks"}
+
+  resources :movies
+  resources :actors
 
   root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
