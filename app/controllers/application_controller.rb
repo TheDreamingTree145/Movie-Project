b322f54dfc20412fb851a9a17d561d9a2a22bea0
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-26 16:37:04 -0400

    logged_in_check method, redirect home page if signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def logged_in_check
+    redirect_to root_path, alert: "You must be signed in to perform this action" unless user_signed_in?
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :current_user, only: [:show]
+  before_action :logged_in_check, only: [:index, :show]
 
   def show
-    redirect_to root_path, alert: "You must be logged in to view your page" unless current_user
+    
   end
 end
