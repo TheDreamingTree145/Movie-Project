eef2f71ae7bd48d971f6e60187fdedeb97859683
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-26 16:47:26 -0400

    added user index page and ability to view others pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   before_action :logged_in_check, only: [:index, :show]
 
+  def index
+    @users = User.all
+  end
+
   def show
-    
+    @user = User.find(params[:id]) # So a logged in user can view a different user's page
   end
 end
