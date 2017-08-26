3d488d5e487fa4c1dad1c20cc72e54f8ec0b2238
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-26 15:08:22 -0400

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+
+@user = User.first
+@user_2 = User.last
+
+5.times do
+  @user.movies.create(title: Faker::Book.title, release_date: rand(1920..2018))
+  @user_2.movies.create(title: Faker::Book.title, release_date: rand(1920..2018))
+end
+
+@movie = Movie.first
+@movie_2 = Movie.last
+
+gender_choice = ["Male", "Female"]
+
+10.times do
+  Actor.create(name: Faker::Name.name, gender: gender_choice.sample)
+end
+
+3.times do
+  @character = @movie.characters.create(name: Faker::Name.first_name, actor_id: Actor.all.sample.id)
+  @character_2 = @movie_2.characters.create(name: Faker::Name.first_name, actor_id: Actor.all.sample.id)
+  @character.save
+  @character_2.save
+end
