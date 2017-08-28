f7690f5a5f89d8780b6111395bdfda1614039bd3
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-28 11:36:19 -0400

    began form, not currently working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,4 +9,18 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+  def create
+    if @movie = Movie.create(movie_params)
+      redirect_to @movie
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :release_date)
+  end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,5 @@ class Movie < ApplicationRecord
   has_many :users, :through => :user_movies
   has_many :characters
   has_many :actors, :through => :characters
+  validates_presence_of :title, :release_date, :critic_rating, :awards
 end
