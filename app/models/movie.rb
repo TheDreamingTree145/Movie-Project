603d5f079e57e9997ec65cf4c812de1d9e471197
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-29 09:54:45 -0400

    add fields to form

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,11 +11,15 @@ class Movie < ApplicationRecord
   def characters_attributes=(characters_attributes)
     self.save
     character_1 = Character.new(name: characters_attributes.values[0]['name'], movie_id: self.id) unless characters_attributes.values[0]['name'].empty?
-    binding.pry
-    character_1.actor = Actor.new(name: characters_attributes.values[0]['actor_attributes']['name'])
-    character_1.save
+    character_1.actor = Actor.new(name: characters_attributes.values[0]['actor_attributes']['name'], gender: characters_attributes.values[0]['actor_attributes']['gender'], age: characters_attributes.values[0]['actor_attributes']['age'])
+    character_2 = Character.new(name: characters_attributes.values[1]['name'], movie_id: self.id) unless characters_attributes.values[1]['name'].empty?
+    character_2.actor = Actor.new(name: characters_attributes.values[1]['actor_attributes']['name'], gender: characters_attributes.values[1]['actor_attributes']['gender'], age: characters_attributes.values[1]['actor_attributes']['age'])
+    character_3 = Character.new(name: characters_attributes.values[2]['name'], movie_id: self.id) unless characters_attributes.values[0]['name'].empty?
+    character_3.actor = Actor.new(name: characters_attributes.values[2]['actor_attributes']['name'], gender: characters_attributes.values[2]['actor_attributes']['gender'], age: characters_attributes.values[2]['actor_attributes']['age'])
     binding.pry
     self.characters << character_1
+    self.characters << character_2
+    self.characters << character_3
     binding.pry
   end
 end
