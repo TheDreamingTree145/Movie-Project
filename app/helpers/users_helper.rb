dbd810d36a62c1f94fd676f50219f6771ed8a545
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-30 13:53:50 -0400

    working on unique_actor method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,14 @@
 module UsersHelper
+
+  def unique_actors
+    actor_count = []
+    current_user.movies.each do |movie|
+      movie.characters.each do |character|
+        if !actor_count.include?(character.actor)
+          actor_count << character.actor
+        end
+      end
+    end
+    actor_count.count
+  end
 end
