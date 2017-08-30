0b0303bab270f2744a839a2d58e91dfabfccb454
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-29 22:06:09 -0400

    fixed association between users and movies on creation

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
   end
 
   def create
-    @movie = current_user.movies.build(movie_params)
+    @movie = current_user.movies.create(movie_params)
     if @movie.save
       redirect_to @movie
     else
