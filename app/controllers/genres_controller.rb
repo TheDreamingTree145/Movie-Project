a76ee01a90774e11e73b68eb108452175e0091ff
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-30 19:57:05 -0400

    added genre show, index, partial

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,11 @@
 class GenresController < ApplicationController
+  before_action :logged_in_check
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
 end
