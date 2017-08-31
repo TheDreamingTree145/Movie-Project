9a73c0aa1b58bb5b29f1b14ba79d70aa86e3f827
Dustin Anderson <dandersonsoftware@outlook.com>
2017-08-31 10:27:23 -0400

    fixed unique_genres helper to not double add

--- app/helpers/genres_helper.rb
+++ app/helpers/genres_helper.rb
@@ -1,2 +1,13 @@
 module GenresHelper
+
+  def unique_genres
+    genre_count = []
+    current_user.movies.each do |movie|
+      if !genre_count.include?(movie.genre.title)
+        genre_count << movie.genre.title
+      end
+    end
+    genre_count.join(", ")
+  end
+
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -12,12 +12,4 @@ module UsersHelper
     actor_count.count
   end
 
-  def unique_genres
-    genre_count = []
-    current_user.movies.collect do |movie|
-      if !genre_count.include?(movie.genre)
-        genre_count << movie.genre.title
-      end
-    end.join(", ")
-  end
 end
