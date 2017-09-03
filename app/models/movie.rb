91c8b5c5ec11e0a7d8a79a13519f68f5a4699152
Dustin Anderson <dandersonsoftware@outlook.com>
2017-09-03 13:39:45 -0400

    Added quote to form and fixed initialization

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,18 +9,19 @@ class Movie < ApplicationRecord
 
   scope :top_rated, -> {where("critic_rating >= 80")}
 
-  def characters_attributes=(characters_attributes)
+  def characters_attributes=(characters_attributes) # Now I'm not sure about this method because database of characters would get huge if they get edit
     characters_attributes.each do |k, v| #values will not work
       character = self.characters.find_or_initialize_by(name: v['name'], movie_id: self.id)
+      character.quote = v['quote']
       if v['actor_id'].empty?
         actor = Actor.new(v['actor_attributes'])
-        binding.pry
         if actor.save
           character.actor = actor
           character.save
         end
       else
         character.actor = Actor.find_by(id: v['actor_id'])
+        character.save
       end
     end
   end
