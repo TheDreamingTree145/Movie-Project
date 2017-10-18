019597fd7eba611845a4836825c906976d806413
dustin <dandersonsoftware@outlook.com>
2017-10-18 15:14:47 -0400

    continued to work on loadGame() almost working

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -4,13 +4,12 @@ function loadCharacters(movie_id) {
     url: `/movies/${movie_id}/characters`,
     dataType: 'json',
     success: function(characters) {
-      if ($('#movie-characters' + characters[0].movie.id).innerHTML) {
-        $('#movie-characters' + characters[0].movie.id).empty();
-        } else {
-          characters.forEach(function(character) {
+      $('#movie-characters-' + characters[0].movie.id).toggle(function() {
+        $('#movie-characters-' + characters[0].movie.id).empty();
+        characters.forEach(function(character) {
             $('#movie-characters-' + character.movie.id).append(`<li>${character.name} Played By: <a href="/actors/${character.actor.id}">${character.actor.name}</li>`)
+          })
         })
       }
-    }
-  })
+    })
 }
