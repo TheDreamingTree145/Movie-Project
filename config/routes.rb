478243ce47a3cee5306002aa7e0b8135ab5730cf
dustin <dandersonsoftware@outlook.com>
2017-10-22 00:20:52 -0400

    added movie details div back to show page. added navbar link

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'welcome#home'
-  get '/movies/random', to: 'movies#random', as: 'random'
+  get '/movies/random', to: 'movies#random', as: 'movies_random'
   post '/movies/:id/add', to: 'movies#add'
   post '/movies/:id/remove', to: 'movies#remove'
   get '/top_rated_movies', to: 'static#top_rated_movies', as: 'top_rated_movies'
